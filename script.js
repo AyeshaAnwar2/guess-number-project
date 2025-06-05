5e62ce6c801deb7e7c4b25b79d4413b9e846312b
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-06-04 17:25:46 -0700

    select all queries

---
+++ script.js
@@ -0,0 +1,10 @@
+const randomnum = parseInt(Math.random() *100 + 1);
+console.log(randomnum)
+
+const button = document.querySelector('#btn');
+const userinput = document.querySelector('#guess-feild');
+const guessslot = document.querySelector('#guesses')
+const remainning = document.querySelector('.remaining')
+const loworhi = document.querySelector('.final')
+const startover = document.querySelector('#result')
+
