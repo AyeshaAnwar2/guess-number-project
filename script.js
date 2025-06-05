b66bc3a6094ed555f6a7295ef0f64b0bfc83974f
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-06-05 12:31:43 -0700

    evaluatin

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let randomnum = parseInt(Math.random() *100 + 1);
-console.log(randomnum)
 
 const button = document.querySelector('#btn');
 const userinput = document.querySelector('#guess-feild');
-const guessslot = document.querySelector('#guesses')
+const guessslot = document.querySelector('.guesses')
 const remainning = document.querySelector('.remaining')
 const loworhi = document.querySelector('.final')
 const startover = document.querySelector('#result')
